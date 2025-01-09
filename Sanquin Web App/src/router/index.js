2d0503615524db659c82b485bde62a601177d83e
robijntje <44802556+robijntje@users.noreply.github.com>
2025-01-09 17:41:13 +0100

    Prepared routing and installed Node/NPM
    Prepared the routing and installed npm and nodejs in the webapp.

--- Sanquin Web App/src/router/index.js
+++ Sanquin Web App/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Informatie from '../views/Informatie.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/informatie',
+      name: 'informatie',
+      component: Informatie,
     },
     {
       path: '/about',
